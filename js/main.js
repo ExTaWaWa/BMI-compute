9e16aa6e0457218a069775d39661a28e2195240d
ExTaWaWa <nienchan@gmail.com>
2021-10-19 21:23:17 +0800

    2021/10/19 add new JS Const
    還在設定變數，功能還沒寫

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 const height = document.querySelector('#height');
 const weight = document.querySelector('#weight');
+const result = document.querySelector('.result');
+const send = document.querySelector('.send');
+const restart = document.querySelector('.restart');
+
+const data = [] || JSON.parse(localStorage.getItem("data"));
+
+function inputBMI(){
+  const cm = height.value;
+  const kg = weight.value;
+  const bmi = Math.round((kg/((cm/100)*(cm/100))*100))/100;
+  const today = new Date();
+  const date = today.getFullYear()+ "-" + (today.getMonth()+1) + "-" + (today.getDate());
+  
+  let str = {};
+
+}
+
+// restart.addEventListener('click' ,);
+// send.addEventListener('click' ,);
